2f1e7da88d447c27611258f6b637b2e58edf9f5d
Daniel Szpisjak <felin.arch@gmail.com>
2016-04-04 20:33:06 +0200

    make hsts mandatory

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ var ssl = require('koa-ssl');
 var helmet = require('koa-helmet');
 var app = koa();
 app.use(ssl({ disabled: false, trustProxy: true }));
-app.use(helmet.hsts({ maxAge: 1, includeSubdomains: true }));
+app.use(helmet.hsts({ maxAge: 1, includeSubdomains: true, force: true }));
 app.use(require('koa-static')(__dirname + '/public/'));
 
 app.listen(process.env.PORT || 3000);
