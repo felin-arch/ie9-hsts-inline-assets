3d82638157adc00cbff7434a66b0c6775b2cba26
Daniel Szpisjak <felin.arch@gmail.com>
2016-04-04 20:24:30 +0200

    add ssl enforcement

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 var koa = require('koa');
+var ssl = require('koa-ssl');
 var app = koa();
+app.use(ssl({ disabled: false, trustProxy: true }));
 app.use(require('koa-static')(__dirname + '/public/'));
 
 app.listen(process.env.PORT || 3000);
