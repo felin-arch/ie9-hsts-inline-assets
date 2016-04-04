3c13b0fb68c53fefae8a05d7219f416729d6e61d
Daniel Szpisjak <felin.arch@gmail.com>
2016-04-04 20:28:15 +0200

    adding helmet

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 var koa = require('koa');
 var ssl = require('koa-ssl');
+var helmet = require('koa-helmet');
 var app = koa();
 app.use(ssl({ disabled: false, trustProxy: true }));
+app.use(helmet.hsts({ maxAge: 1, includeSubdomains: true }));
 app.use(require('koa-static')(__dirname + '/public/'));
 
 app.listen(process.env.PORT || 3000);
